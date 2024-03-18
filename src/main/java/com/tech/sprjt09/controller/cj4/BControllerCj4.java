40bffa6718ba8fb3e280286482ed84853fd5d9a6
cpu624049 <kjs940426@naver.com>
2024-03-18 15:54:49 +0900

    kang1 comment

--- src/main/java/com/tech/sprjt09/controller/cj4/BControllerCj4.java
+++ src/main/java/com/tech/sprjt09/controller/cj4/BControllerCj4.java
@@ -15,12 +15,14 @@ import com.tech.sprjt09.service.cj3.BModifyService;
 @Controller
 public class BControllerCj4 {
 	BServiceInter bServiceInter;
+	
+	// kang1 comment
 
 	@RequestMapping("/delete")
 	public String delete(HttpServletRequest request,
 			Model model) {
 		System.out.println("delete()");
-////		db¿¡ µ¥ÀÌÅÍ »èÁ¦
+////		dbï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
 //		//toss
 //		model.addAttribute("request",request);
 //		bServiceInter=new BDeleteService();
